c722c422ae3d9283ee6666fdd1b89b3393467106
Eliasith Trujillo Cortes <eliasithtrujillocortes@MacBook-Air-de-Eliasith.local>
2022-10-28 16:44:22 -0500

    Iniciar

---
+++ Num_Primos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Num_Primos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            inicial();
+        }
+
+        static void inicial()
+        {
+            Console.Clear();
+            do
+            {
+                int num1;
+                Console.Write("Ingrese un número: ");
+                num1 = int.Parse(Console.ReadLine());
+         
+                if ( num1 % 2 <= num1)
+                {
+                        Console.WriteLine($"El numero {num1} es PRIMO ");
+                }
+                else if (num1 % 2 == 1)
+                    Console.WriteLine($"El numero NO es Primo");
+
+
+            } while (true);
+        }
+    }
+}
+
+
+
---
+++ Promedio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Promedio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
